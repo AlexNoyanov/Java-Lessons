5f42ea0ded0007df8760eece08100f54c2fff0fd
Alex Noyanov <alex.noyanov@gmail.com>
2020-02-14 22:12:19 +0300

    Java example lesson about Classes

--- Refereces_and_Primitives/src/com/company/Main.java
+++ Refereces_and_Primitives/src/com/company/Main.java
@@ -21,6 +21,13 @@ public class Main {
         int million1 = 1000000;     // Million
         int million2 = 1_000_000;   //  You can use underscore for a million
 
+
+        boolean b1, b2;             // This is OK
+        String s1 = "1", s2;        // Fine too
+       // double d1, double d2;       // ILLEGAL!
+        int i1; int i2;               // OK
+        //int i3; i4;                 // Of corse this is ILLEGAL
+
         double annouingNumbers = 1_00_0.0_0;    // Double value formatting
 
 
@@ -30,6 +37,21 @@ public class Main {
         double notByDecimal = 1000_.00; // DOES NOT COMPILE
          */
 
+        /*
+            There are only three rules to remember for legal identifi ers:
+            ■ The name must begin with a letter or the symbol $ or _.
+            ■ Subsequent characters may also be numbers.
+            ■ You cannot use the same name as a Java reserved word. As you might imagine, a
+            reserved word is a keyword that Java has reserved so that you are not allowed to use it.
+            Remember that Java is case sensitive, so you can use versions of the keywords that only
+            differ in case. Please don’t, though.
+         */
+
+        int  $h;        // OK
+        int  aAaaA;     // FINE
+        int _good;      // why not? still good
+        // int 3a;      // NOO..
+        // int *coffe; NOT a letter, $ or _
 
         // Now let's take a look at the Referrences:
         // A reference can be assigned to another object of the same type.
