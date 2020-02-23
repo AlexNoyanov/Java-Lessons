0e8fe5b8fedb70b4bbec849a6e7c262b483a87e7
Alex Noyanov <alex.noyanov@gmail.com>
2020-02-23 21:07:27 +0300

    Some basics about classes in Java

--- ClassVariables/src/com/company/MyClass.java
+++ ClassVariables/src/com/company/MyClass.java
@@ -1,4 +1,32 @@
-package com.company;
+// Lesson about class variables and
+// main elements of the class
 
-public class MyClass {
+
+// PACKAGE always before IMPORT!
+
+package com.company;        // Package declaration (First)
+
+// import        (second)
+
+// To memorize it use the acronim PIC - Package,Impoort,Class
+
+// Multiple classes can be declared in one file but only one of then can be public
+
+public class MyClass {  // Class declaration
+    int value;          // Field declaration
+    int x1,x2;
+
+    int sumValues(){
+        return  x1+x2;
+    }
+
+}
+
+// This classes are private and can be declared here:
+class myPrivateClass1{
+
+}
+
+class myPrivateClass2{
+
 }
---
+++ ClassVariables/src/com/company/PrivateClass.java
@@ -0,0 +1,16 @@
+package com.company;
+
+
+// All of the classes in this file are private
+// And this is OK!
+
+class PrivateClass {
+
+
+
+}
+
+
+class secondClass{
+
+}
