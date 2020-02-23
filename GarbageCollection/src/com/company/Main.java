72284d3fa93c3c854e8d4c5ba1e4e5146d25dad4
Alex Noyanov <alex.noyanov@gmail.com>
2020-02-23 22:26:13 +0300

    Committing all changes in this lessons

---
+++ GarbageCollection/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+// Lesson about Garbage Collector in Java Language
+
+
+/*
+
+    An object is no longer reachable when one of two situations occurs:
+    ■ The object no longer has any references pointing to it.
+    ■ All references to the object have gone out of scope.
+
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    // To tell what this is a good time for garbage collector to run:
+    // System.gc();  Sometimes in wouldn't even work
+
+
+    }
+}
