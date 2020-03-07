030eeb48f867442da22691d645f51dd049e9b2dc
Alex Noyanov <alex.noyanov@gmail.com>
2020-03-07 23:59:04 +0300

    Another Java lesson

---
+++ Logical_operators/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+// The lesson about logical operators
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    String x = "Hello";
+    boolean y = false;
+        //boolean x = true || (y < 4);
+        //System.out.println(x);
+        if(x != null && x.getValue() < 5) {
+// Do something
+        }
+    }
+}
