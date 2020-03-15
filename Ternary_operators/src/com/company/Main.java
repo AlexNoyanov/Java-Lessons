02192765f457e11103309e80a245ab90ec31607b
Alex Noyanov <alex.noyanov@gmail.com>
2020-03-15 09:39:55 +0300

    Ternary operators

--- Ternary_operators/src/com/company/Main.java
+++ Ternary_operators/src/com/company/Main.java
@@ -26,6 +26,21 @@ public class Main {
         int yy = 10;
         int xx = (yy > 5) ? (2 * y) : (3 * y);
 
+        int yyy = 6;
+
+        System.out.println((yyy > 5) ? 21 : "Zebra");
+
+        // int animal = (y < 91) ? 9 : "Horse"; // DOES NOT COMPILE (Can't use String data type for int)
+
+        // Another example for if and ternary expression:
+        if( x == 5){
+            System.out.println("x = 5");
+        }else{
+            System.out.println(x);
+        }
+
+        System.out.println((x==5) ? "x = 5" : x);   // This is equal to IF code writen before
+
 
 
     }
