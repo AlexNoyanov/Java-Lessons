24b391c539a71fd7154ffc78c00f443f058773e4
Alex Noyanov <alex.noyanov@gmail.com>
2020-04-04 00:06:50 +0300

    Converting String to the int

---
+++ String_to_Int_Generate/src/com/alex/Main.java
@@ -0,0 +1,24 @@
+package com.alex;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        String link = "https://scontent-frx5-1.cdninstagram.com/v/t51.2885-15/e35/91127262_840241943112072_2111894168306547242_n.jpg?_nc_ht=scontent-frx5-1.cdninstagram.com&_nc_cat=100&_nc_ohc=xogMW5IUVbkAX815zSb&oh=6119e278c187bd08727bbe6374f82e51&oe=5E89DBCC";
+
+        int convertedLink = 0;
+
+        String simpleStr = "";
+        String code = "";
+
+        for(char c : simpleStr.toCharArray()){
+            System.out.print("Symbol  ");
+            System.out.print(c);
+            System.out.print(" = ");
+            System.out.println((int)c);
+
+        }
+
+    }
+}
