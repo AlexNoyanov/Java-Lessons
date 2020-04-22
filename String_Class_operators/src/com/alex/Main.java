ba798948ff0cab7f396b3a3d46f6740e2b02ddce
Alex Noyanov <alex.noyanov@gmail.com>
2020-04-22 17:11:19 +0300

    Working with String in Java language

---
+++ String_Class_operators/src/com/alex/Main.java
@@ -0,0 +1,37 @@
+package com.alex;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+    Lesson about operations with String class in Java
+
+ By Alexander Noyanov
+ April 22nd 2020
+
+
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        LocalDate dateNow = LocalDate.now();
+
+        String currentDate = now.toString();
+
+        int indexOfTime = currentDate.indexOf('T');
+        String dateHour = currentDate.substring(0,currentDate.indexOf('T'));
+        LocalTime timeNow = LocalTime.now();
+
+        System.out.println("Full date and time :" + currentDate);
+        System.out.println("Just date here: "+dateNow);
+        System.out.println("Just time: " +  timeNow);
+
+    }
+}
